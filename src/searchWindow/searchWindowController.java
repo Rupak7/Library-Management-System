a858b021d2b9c86c6693b1f640799e15ed7e338e
Rupak.joko7 <rupakkalita3@gmail.com>
2018-10-02 17:04:26 +0530

    Update searchWindowController.java

--- src/searchWindow/searchWindowController.java
+++ src/searchWindow/searchWindowController.java
@@ -8,45 +8,18 @@
 
 
     import javafx.beans.property.SimpleStringProperty;
-    import DButils.ConnectDB;
-    import java.io.IOException;
-    import java.net.URL;
-    import java.sql.Connection;
-    import java.sql.PreparedStatement;
-    import java.sql.ResultSet;
-    import java.sql.SQLException;
-    import java.util.Optional;
-    import java.util.ResourceBundle;
-    import javafx.collections.FXCollections;
-    import javafx.collections.ObservableList;
-    import javafx.event.ActionEvent;
-    import javafx.fxml.FXML;
-    import javafx.fxml.FXMLLoader;
-    import javafx.fxml.Initializable;
-    import javafx.scene.Parent;
-    import javafx.scene.Scene;
-    import javafx.scene.control.Alert;
-    import javafx.scene.control.ButtonType;
-    import javafx.scene.control.Label;
-    import javafx.scene.control.ListView;
     import javafx.scene.control.TextField;
-    import javafx.scene.image.Image;
-    import javafx.stage.Stage;
-    import javafx.stage.StageStyle;
     import javafx.scene.control.TableColumn;
     import javafx.scene.control.cell.PropertyValueFactory;
-    import java.sql.*;
     import java.util.logging.Level;
     import java.util.logging.Logger;
     import javafx.scene.control.TableView;
     import DButils.ConnectDB;
-    import java.io.IOException;
     import java.net.URL;
     import java.sql.Connection;
     import java.sql.PreparedStatement;
     import java.sql.ResultSet;
     import java.sql.SQLException;
-    import java.util.Optional;
     import java.util.ResourceBundle;
     import javafx.collections.FXCollections;
     import javafx.collections.ObservableList;
@@ -255,7 +228,7 @@
                 }
 
 
-                public String getMobile() {
+                public String getDue() {
                     return due.get();
                 }
 
